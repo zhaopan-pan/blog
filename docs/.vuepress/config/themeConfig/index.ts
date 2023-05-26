723cc0d55f7a993feb09634ca5b1b1d5a97a2a7e
zhaopan <zhaopanhw@foxmail.com>
2023-05-26 18:54:52 +0800

    feat: update theme、add external link

--- docs/.vuepress/config/themeConfig/index.ts
+++ docs/.vuepress/config/themeConfig/index.ts
@@ -14,6 +14,18 @@ export const themeConfig = (options?: ZpThemeOptions) => ({
 		homeTopBg: '',
 		// 'https://images.pexels.com/photos/1764702/pexels-photo-1764702.jpeg?auto=compress&cs=tinysrgb&w=1600',
 		// 'https://images.pexels.com/photos/3695297/pexels-photo-3695297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+		externalLinkList: [
+			{
+				icon: 'https://ts4.cn.mm.bing.net/th?id=ODLS.fe916855-7a5a-4f44-b52e-1c5c31f0329d&w=24&h=24&o=6&pid=1.2',
+				text: 'github',
+				url: 'https://github.com/zhaopan-pan/vuepress-theme-zp',
+			},
+			{
+				icon: 'https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/6c61ae65d1c41ae8221a670fa32d05aa.svg',
+				text: '掘金',
+				url: 'https://juejin.cn/user/4292955583560968',
+			},
+		],
 	},
 	locales: {
 		'/': {
