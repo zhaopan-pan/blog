7a58522b0eadec19a5f0b8ff0eae1df74b100661
zhaopan <zhaopanhw@foxmail.com>
2023-08-09 22:26:37 +0800

    feat: 统一 页面 description长度

--- docs/.vuepress/config/plugin/seo.ts
+++ docs/.vuepress/config/plugin/seo.ts
@@ -1,4 +1,4 @@
-import {seoPlugin} from 'vuepress-plugin-seo2'
+import {ExtendPage, SeoContent, seoPlugin} from 'vuepress-plugin-seo2'
 import {sitemapPlugin} from 'vuepress-plugin-sitemap2'
 
 export default ({hostname, title, email}) => [
@@ -20,6 +20,18 @@ export default ({hostname, title, email}) => [
 			 * 作者 Email
 			 */
 			email,
+		},
+		ogp: (ogp: SeoContent, page) => {
+			// bing-webmasters：将页面源代码的 <meta description> 标记中的描述长度更改为介于 25 到 160 个字符之间。
+			// 暂取120
+			const description = (
+				page.frontmatter.description || ogp['og:description']
+			)?.substring(0, 120)
+			// 统一 description
+			page.frontmatter.description = ogp[
+				'og:description'
+			] = `${description}...`
+			return ogp
 		},
 	}),
 ]
--- docs/home/index.md
+++ docs/home/index.md
@@ -4,10 +4,6 @@ excerpt: false
 sidebar: false
 date: 2022-12-02
 author: 幻无
-head:
-  - - meta
-    - name: description
-      content: 幻无的blog，记录工作、学习、生活过程中的一些思考，主要包括技术、生活等方面
 ---
 
 ::: tip 介绍
