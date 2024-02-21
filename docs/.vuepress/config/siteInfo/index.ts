3124ff1d204d8d1024733f78fc1d33b325b1b6ba
zhaopan <zhaopanhw@foxmail.com>
2024-02-21 23:00:02 +0800

    chore: 更新版本

--- docs/.vuepress/config/siteInfo/index.ts
+++ docs/.vuepress/config/siteInfo/index.ts
@@ -3,8 +3,8 @@
  */
 const title = '幻无'
 export default {
-	title,
-	description: `${title}的blog，记录工作、学习、生活过程中的一些思考，主要包括技术、生活等方面`,
+	title: `${title}的小屋`,
+	description: `记录工作、学习、生活过程中的一些思考`,
 	hostname: 'https://zhaopanpan.com',
 	email: 'zhaopanhw@foxmail.com',
 }
--- docs/README.md
+++ docs/README.md
@@ -2,9 +2,9 @@
 home: true
 readme: true
 footer: footer
-title: 幻无的blog，记录工作、学习、生活过程中的一些思考，主要包括技术、生活等方面
+title: 首页
 head:
   - - meta
     - name: description
-      content: 幻无的blog，记录工作、学习、生活过程中的一些思考，主要包括技术、生活等方面
+      content: 记录工作、学习、生活过程中的一些思考
 ---
